9ddc61b520a834edec3c8e92cd0727ef56ceb8db
Raphael Ohlsen <raphael.ohlsen@gmail.com>
2020-02-13 13:47:59 -0300

    Criada rota para criaçao de professores, Inicio de criação de formulário até Grau de Escolaridade
    Inicio de criação de formulário até Grau de Escolaridade

--- routes.js
+++ routes.js
@@ -10,6 +10,10 @@ routes.get("/teachers", (req,res) => {
   return res.render("teachers/index");
 });
 
+routes.get("/teachers/create", (req,res) => {
+  return res.render("teachers/create");
+})
+
 routes.get('/teachers/:id', teachers.show)
 
 routes.get('/students', (req, res) => {
